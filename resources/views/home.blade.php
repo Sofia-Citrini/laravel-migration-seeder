1df271c4d2a11381003a712e8af4dcfdd2e3cc68
Sofia-Citrini <sofiacitrini@gmail.com>
2023-01-24 18:08:37 +0100

    added more elements in cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,10 @@
 <div class="row justify-content-center">
     <div class="col-7">
         @foreach ($trains as $train)
+            <div>Partenza il {{date("d/m/Y", strtotime($train->departure_time))}} </div>
+
             <div class="card mb-3">
-                <div class="card-header">{{$train->company}}</div>
+                <div class="card-header">{{$train->company}} - {{$train->train_code}}</div>
 
                 <div class="card-body d-flex justify-conten-between">
                     <div class="flex-fill">
@@ -18,6 +20,14 @@
                         <h5>{{$train->arrival_station}}</h5>
                     </div>
                 </div>
+
+                @if($train->on_time === 1)
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        in ritardo
+                    </span>
+                @endif
+                
+                
             </div>
         @endforeach
     </div>
